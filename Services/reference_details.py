d9d1c4773cc0b2ef0260402bfbca81db31d9eb11
albertor03 <alberto.zapata@kushkipagos.com>
2022-12-27 11:39:02 -0500

    validate

--- Services/reference_details.py
+++ Services/reference_details.py
@@ -1,20 +1,37 @@
+import json
+
+from Services.validate import ValidateISO
+
+
 class ISO(object):
 
     def __init__(self):
         self.params = str()
         self.__msgType = {"length": 4, "start": 0, "end": 4}
-        self.__primaryBitMap = {"length": 16, "start": 4, "end": 20}
-        self.__secondBitMap = {"length": 16, "start": 20, "end": 36}
         self.__processingCode = {"length": 6, "start": 55, "end": 61}
-        self.__timeLocalTrx = {"length": 6, "start": 79, "end": 85}
-        self.__dateLocalTrx = {"length": 8, "start": 85, "end": 93}
-        self.__transactionCurrency = {"length": 3, "start": 189, "end": 192}
-        self.__pll = {"length": 4, "start": 192, "end": 196}
+
+        self.__codErrorOriPosition = {"length": 3, "start": 171, "end": 174}
+        self.__codErrorOri = '000'
+
+        self.__valid = ValidateISO()
+
+        iso_file = open("./iso.json", "r")
+        self.__consultRespISO = json.loads(iso_file.read())['consultRespISO']
 
     def consult(self, params: str):
         self.params = self.__parse_iso(params)
 
     def __parse_iso(self, iso: str):
-        msg = "0210"
-        iso = iso.replace(iso[self.__msgType["start"]:self.__msgType["end"]], msg, 1)
+        is_valid = self.__valid.validate_iso(iso)
+        if is_valid != '000':
+            self.__codErrorOri = is_valid
+
+        iso = iso.replace(iso[self.__msgType['start']:self.__msgType['end']], "0210", 1)
+        pro = iso[self.__processingCode['start']:self.__processingCode['end']]
+
+        if pro != '310000':
+            self.__codErrorOri = '002'
+
+        iso = self.__consultRespISO.replace("ERRORCODE", self.__codErrorOri, 1)
+
         return iso
---
+++ Services/validate.py
@@ -0,0 +1,67 @@
+import datetime
+
+
+class ValidateISO(object):
+
+    def __init__(self):
+        self.__msgType = {"length": 4, "start": 0, "end": 4}
+        self.__primaryBitMap = {"length": 16, "start": 4, "end": 20}
+        self.__secondBitMap = {"length": 16, "start": 20, "end": 36}
+        self.__processingCode = {"length": 6, "start": 55, "end": 61}
+        self.__timeLocalTrx = {"length": 6, "start": 79, "end": 85}
+        self.__dateLocalTrx = {"length": 8, "start": 85, "end": 93}
+        self.__transactionCurrency = {"length": 3, "start": 189, "end": 192}
+        self.__pll = {"length": 4, "start": 192, "end": 196}
+        self.__codErrorOri = ''
+
+    def validate_iso(self, iso: str):
+        code = '000'
+        msg = iso[self.__msgType['start']:self.__msgType['end']]
+        primary = iso[self.__primaryBitMap['start']:self.__primaryBitMap['end']]
+        second = iso[self.__secondBitMap['start']:self.__secondBitMap['end']]
+        currency = iso[self.__transactionCurrency['start']:self.__transactionCurrency['end']]
+
+        other_code = self.__validate_other(msg, primary, second, currency)
+        if other_code:
+            return other_code
+
+        date_code = self.__validate_date(str(iso[self.__dateLocalTrx['start']:self.__dateLocalTrx['end']]))
+        if date_code:
+            return date_code
+
+        time_code = self.__validate_time(iso[self.__timeLocalTrx['start']:self.__timeLocalTrx['end']])
+        if time_code:
+            return time_code
+
+        return code
+
+    def __validate_other(self, msg: str, primary: str, second: str, currency: str) -> str:
+        code = self.__codErrorOri
+
+        if msg != '0200' or primary != 'F038048188E00000' or second != '0000000000000080' or currency != '604':
+            return '002'
+
+        return code
+
+    def __validate_date(self, date: str) -> str:
+        code = self.__codErrorOri
+
+        try:
+            date = str(datetime.datetime.strptime(f"{date[0:2]}-{date[2:4]}-{date[4:8]}", '%d-%m-%Y'))
+        except ValueError:
+            return '061'
+
+        if datetime.datetime.strptime(date, '%Y-%m-%d %H:%M:%S') > datetime.datetime.now():
+            return '061'
+
+        return code
+
+    def __validate_time(self, time: str) -> str:
+        code = self.__codErrorOri
+
+        try:
+            datetime.datetime.strptime(f"{time[0:2]}:{time[2:4]}:{time[4:6]}", '%H:%M:%S')
+        except ValueError:
+            return '060'
+
+        return code
